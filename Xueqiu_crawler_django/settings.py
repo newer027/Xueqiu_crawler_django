5029041448200c77907e3f5d62065dd421685ddc
cheng-jian <newer027@gmail.com>
2017-03-24 17:22:39 +0800

    href

--- Xueqiu_crawler_django/settings.py
+++ Xueqiu_crawler_django/settings.py
@@ -18,10 +18,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'crawler/static'),
-#)
-#STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, "crawler/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
