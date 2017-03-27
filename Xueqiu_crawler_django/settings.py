4f2452276e480087976b3550c589a97dc6ec5757
cheng-jian <cj@gzqichang.com>
2017-03-27 18:10:19 +0800

    local

--- Xueqiu_crawler_django/settings.py
+++ Xueqiu_crawler_django/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'Xueqiu_crawler_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crawler',
+        'USER': 'jacob',
+        'PASSWORD': 'ML#$gbdb',
+        'HOST': '127.0.0.1',
+	'PORT': '3306',
     }
 }
 
